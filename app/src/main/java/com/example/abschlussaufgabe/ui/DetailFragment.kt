7c4a9026838d1a3b82173e985909af0a247ec370
dannywilliams <dakewi@hotmail.de>
2023-07-19 15:43:16 +0200

    Detail Fragment hinzugefügt und Designt

---
+++ app/src/main/java/com/example/abschlussaufgabe/ui/DetailFragment.kt
@@ -0,0 +1,45 @@
+package com.example.abschlussaufgabe.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import com.example.abschlussaufgabe.R
+import com.example.abschlussaufgabe.databinding.FragmentDetailBinding
+import com.example.abschlussaufgabe.databinding.FragmentSortimentBinding
+import com.example.abschlussaufgabe.viewmodel.MainViewModel
+
+
+class DetailFragment : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
+    private lateinit var binding: FragmentDetailBinding
+
+
+    /**
+     * Hier in der OnStart wird die HideNavigation & HideToolbar einblendet
+     */
+    override fun onStart() {
+        super.onStart()
+        viewModel.hideNavigation(false)
+        viewModel.hideToolbar(false)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_detail,container,false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
