426bf625860c7ef976d8c385f95c66e44650526e
dannywilliams <dakewi@hotmail.de>
2023-09-06 10:45:08 +0200

    Letzter Stand vor AbschlussPrüfung

--- app/src/main/java/com/example/abschlussaufgabe/ui/AdressenFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/AdressenFragment.kt
@@ -10,11 +10,6 @@ import androidx.navigation.fragment.findNavController
 import com.example.abschlussaufgabe.R
 import com.example.abschlussaufgabe.databinding.FragmentAdressenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 
 class AdressenFragment : Fragment() {
 
